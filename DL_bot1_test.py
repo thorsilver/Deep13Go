30642a957b83d43071534c3567c4bff3022e05b6
Dr Eric Silverman <thorsilver@gmail.com>
2019-03-19 00:49:47 +0000

    v0.4 -- second attempt

--- DL_bot1_test.py
+++ DL_bot1_test.py
@@ -1,7 +1,10 @@
 import h5py
+from keras.models import Sequential
+from keras.layers import Dense
 
 from dlgo.agent.predict import DeepLearningAgent, load_prediction_agent
 from dlgo.httpfrontend import get_web_app
+from dlgo.networks import large
 
 model_file = h5py.File("checkpointsOneLarge\large_model_epoch_8.h5", "r")
 bot_from_file = load_prediction_agent(model_file)
